bfa387d3d0a4d9512a3c304030f2ff9aa8165f42
nicolasgavriil <219146079+nicolasgavriil@users.noreply.github.com>
2026-04-29 15:14:04 +0000

    Chapter 2 Exercise 4

--- library-backend/index.js
+++ library-backend/index.js
@@ -98,7 +98,7 @@ const typeDefs = /* GraphQL */ `
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String): [Book!]!
     allAuthors: [Author!]!
   }
 `;
@@ -107,7 +107,7 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) => books.filter((b) => b.author === args.author),
     allAuthors: () => authors,
   },
 
