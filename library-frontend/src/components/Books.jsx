59568d32c7bee0a84cabfab27efca1196c267074
nicolasgavriil <219146079+nicolasgavriil@users.noreply.github.com>
2026-05-03 11:39:45 +0000

    Chapter 5 Exercise 20

--- library-frontend/src/components/Books.jsx
+++ library-frontend/src/components/Books.jsx
@@ -1,16 +1,25 @@
 import { useQuery } from "@apollo/client/react";
+import { useState } from "react";
 import { ALL_BOOKS } from "../queries.js";
 
-const Books = (props) => {
+const Books = ({ show }) => {
+  const [filter, setFilter] = useState();
   const result = useQuery(ALL_BOOKS);
 
-  if (!props.show) return null;
+  if (!show) return null;
 
   if (result.loading) return null;
 
   const books = result.data.allBooks;
 
   if (!books) return null;
+
+  const genres = new Set();
+  for (const book of books) {
+    book.genres.forEach((genre) => {
+      genres.add(genre);
+    });
+  }
 
   return (
     <div>
@@ -23,15 +32,22 @@ const Books = (props) => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {books.map((a) => (
-            <tr key={a.id}>
-              <td>{a.title}</td>
-              <td>{a.author.name}</td>
-              <td>{a.published}</td>
-            </tr>
-          ))}
+          {books
+            .filter((b) => b.genres.includes(filter))
+            .map((a) => (
+              <tr key={a.id}>
+                <td>{a.title}</td>
+                <td>{a.author.name}</td>
+                <td>{a.published}</td>
+              </tr>
+            ))}
         </tbody>
       </table>
+      {[...genres].sort().map((g) => (
+        <button key={g} type="button" onClick={() => setFilter(g)}>
+          {g}
+        </button>
+      ))}
     </div>
   );
 };
